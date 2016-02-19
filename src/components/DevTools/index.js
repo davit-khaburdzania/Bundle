29139bea4db736ca5b2395d343f6ac6455a23ffd
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-02-19 20:21:01 +0400

    added slider monitor devtools 🕶

--- src/components/DevTools/index.js
+++ src/components/DevTools/index.js
@@ -3,11 +3,15 @@ import { createDevTools } from 'redux-devtools'
 
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+import SliderMonitor from 'redux-slider-monitor'
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q'
-    defaultIsVisible={false}
+  <DockMonitor
+    toggleVisibilityKey='ctrl-h'
+    changePositionKey='ctrl-q'
+    changeMonitorKey='ctrl-m'
   >
     <LogMonitor expandActionRoot={false} />
+    <SliderMonitor keyboardEnabled />
   </DockMonitor>
 )
