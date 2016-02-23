0f7578ce564dbc310100bac7aebac7055e52f277
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-02-23 17:59:12 +0400

    moved root component to containers

--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -3,34 +3,17 @@ import { Provider } from 'react-redux'
 import { Router, Route, IndexRedirect } from 'react-router'
 
 import DevTools from './../../utils/DevTools'
-
-import createStore from './../../store/createStore'
 import history from './../../utils/history'
+import routes from './../../routes'
+import createStore from './../../store/createStore'
 
 let store = createStore({})
-
-import {
-  App,
-  Bundles,
-  Collections,
-  Favorites,
-  Notifications
-} from '../'
 
 export default function Root () {
   return (
     <Provider store={store}>
       <div className='root-container'>
-        <Router history={history}>
-          <Route path='/' component={App}>
-            <IndexRedirect to='/bundles' />
-
-            <Route path='/bundles' component={Bundles} />
-            <Route path='/collections' component={Collections} />
-            <Route path='/favorites' component={Favorites}/>
-            <Route path='/notifications' component={Notifications} />
-          </Route>
-        </Router>
+        <Router history={history} routes={routes} />
         <DevTools/>
       </div>
     </Provider>
