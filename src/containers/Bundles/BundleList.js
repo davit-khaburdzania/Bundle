e6058f2aa9066e58a4386d1c922cee10ca31bbb8
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-02-23 19:43:56 +0400

    use decorators for connect

---
+++ src/containers/Bundles/BundleList.js
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react'
+import List from './../../components/List'
+import { Search } from '..'
+import BundleListItem from './../../components/BundleListItem'
+
+export default function BundleList ({
+  bundles,
+  search,
+  dispatch,
+  toggleSearchVisibility,
+  getSearchResult
+}) {
+  let styles = { 'display': search.open ? 'none' : 'block' }
+
+  let currentListItems = search.result.length ? search.result : bundles
+
+  return (
+    <div className='bundle-container'>
+      <div className='top-nav'>
+        <h2 style={styles} className='title'>Bundles</h2>
+        <div className='nav'>
+          <Search search={search.open}
+           onClick={toggleSearchVisibility}
+           onChange={getSearchResult}
+          />
+        </div>
+      </div>
+
+      <List>
+        {currentListItems.map((bundle, index) =>
+          <List.Item key={index}
+            {...bundle}
+            Component={BundleListItem}
+          />
+        )}
+      </List>
+    </div>
+  )
+}
--- src/containers/Bundles/index.js
+++ src/containers/Bundles/index.js
@@ -2,63 +2,35 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators as ba } from 'redux'
 import * as bundleActions from '../../actions/Bundle'
-import * as SearchActions from '../../actions/SearchContainer'
+import * as searchActions from '../../actions/SearchContainer'
 
-import List from './../../components/List'
-import BundleListItem from './../../components/BundleListItem'
-import { Search } from '../'
+import BundleList from './BundleList'
 
 import './style.css'
 
-function BundleList ({
-  bundles,
-  search,
-  dispatch,
-  toggleSearchVisibility,
-  getSearchResult
-}) {
-
-  let styles = { 'display': search.open ? 'none' : 'block' }
-
-  let currentListItems = search.result.length ? search.result : bundles
-
-  return (
-    <div className='bundle-container'>
-      <div className='top-nav'>
-        <h2 style={styles} className='title'>Bundles</h2>
-        <div className='nav'>
-          <Search search={search.open}
-           onClick={toggleSearchVisibility}
-           onChange={getSearchResult}
-          />
-        </div>
-      </div>
-
-      <List>
-        {currentListItems.map((bundle, index) =>
-          <List.Item key={index}
-            {...bundle}
-            Component={BundleListItem}
-          />
-        )}
-      </List>
-    </div>
-  )
-}
-
-class BundleListContainer extends Component {
+@connect(
+  state => ({
+    bundles: state.Bundle.list,
+    search: state.Search
+  }), {
+     ...bundleActions,
+     ...searchActions
+})
+export default class BundleListContainer extends Component {
   constructor (props) {
-    props.dispatch(bundleActions.getBundles())
-
+    props.getBundles()
     super(props)
   }
 
   render () {
-    let { bundles, search, dispatch } = this.props
-    let boundActionCreators = ba(SearchActions, dispatch)
+    let { bundles, search} = this.props
+    let actions = {
+      toggleSearchVisibility: this.props.toggleSearchVisibility,
+      getSearchResult: this.props.getSearchResult
+    }
 
     return (
-      <BundleList {...boundActionCreators} search={search} bundles={bundles} />
+      <BundleList {...actions} search={search} bundles={bundles} />
     )
   }
 }
@@ -68,10 +40,3 @@ BundleListContainer.propTypes = {
   search: PropTypes.object,
   dispatch: PropTypes.func
 }
-
-export default connect((state) => {
-  return {
-    bundles: state.Bundle.list,
-    search: state.Search
-  }
-})(BundleListContainer)
