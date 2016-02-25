5436f64f70dfdb3d7421d18f9e029621a71f91b9
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-02-25 15:15:28 +0400

    add alert action

--- src/actions/Alerts.js
+++ src/actions/Alerts.js
@@ -1,3 +1,8 @@
+export function addAlert (type, list) {
+  if (typeof list == 'string') list = [list]
+  return { type: 'ADD_ALERT', alert: { type, list } }
+}
+
 export function removeAlert () {
   return { type: 'REMOVE_ALERT' }
 }
