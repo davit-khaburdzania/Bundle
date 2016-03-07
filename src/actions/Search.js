c8ba6e05e814d4228c3d8a4f43f08710724b659d
gulisashvili <levan.gulisashvili@gmail.com>
2016-03-07 15:52:15 +0400

    refactorings

--- src/actions/Search.js
+++ src/actions/Search.js
@@ -3,16 +3,14 @@ import api from './../api'
 
 export function getSearchResult (value) {
   return async (dispatch) => {
-    if (value) {
-      try {
-        let response = await request.get(api.search(value))
-        dispatch({ type: 'FETCH_SEARCH_RESULTS', result: response.data })
-      } catch (error) {
-        // write error handler outside
-        console.log(error)
-      }
-    } else {
-      return dispatch({ type: 'FETCH_SEARCH_RESULTS' })
+    if (!value) return dispatch({ type: 'FETCH_SEARCH_RESULTS' })
+    
+    try {
+      let response = await request.get(api.search(value))
+      dispatch({ type: 'FETCH_SEARCH_RESULTS', result: response.data })
+    } catch (error) {
+      // write error handler outside
+      console.log(error)
     }
   }
 }
--- src/containers/Search/body/index.js
+++ src/containers/Search/body/index.js
@@ -8,7 +8,7 @@ import {
 import './index.css'
 
 function isAnyResult (searchResults) {
-  return isBundles(searchResults) || isCollections(searchResults)
+  return searchResults && (isBundles(searchResults) || isCollections(searchResults))
 }
 
 function isBundles (searchResults) {
@@ -24,43 +24,40 @@ function shouldShow (show) {
 }
 
 function renderResults (searchResults) {
-  if (searchResults && isAnyResult(searchResults)) {
-    return (
-      <div className='search-results'>
-        <h3 className='title'> Search results </h3>
+  if (! isAnyResult(searchResults)) {
+    return <div className="search-note"> Search Bundles and Collections </div>
+  }
 
-        <List>
-        <h4 className='name'
-        style={shouldShow(isCollections(searchResults))}>
-        Collections
+  return (
+    <div className='search-results'>
+      <h3 className='title'> Search results </h3>
+
+      <List>
+        <h4 className='name' style={shouldShow(isCollections(searchResults))}>
+          Collections
         </h4>
         {searchResults.collections.map((item, index) =>
           <ListItem key={index} {...item} Component={ListItem.Collection} />
         )}
-        </List>
+      </List>
 
-        <List>
-        <h4 style={shouldShow(isBundles(searchResults))}
-        className='name'> Bundles </h4>
+      <List>
+      <h4 style={shouldShow(isBundles(searchResults))}
+          className='name'> Bundles </h4>
         {searchResults.bundles.map((item, index) =>
           <ListItem key={index} {...item} Component={ListItem.Bundle} />
         )}
-        </List>
+      </List>
 
-      </div>
-    )
-  } else {
-    return (
-      <div className="search-note"> Search Bundles and Collections </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default function SearchBody ({ searchResults }) {
   return (
     <div className='search-results-wrapper'>
 
-    { renderResults(searchResults) }
+      { renderResults(searchResults) }
 
     </div>
   )
