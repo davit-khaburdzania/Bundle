5c70e9cf1d87b2bdf51b903966d58b7ccd370413
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-03-10 21:59:54 +0400

    working bundle favorite

--- src/actions/Favorite.js
+++ src/actions/Favorite.js
@@ -4,13 +4,19 @@ import api from './../api'
 export function favorite (resource, id) {
   return async (dispatch) => {
     let response = await request.post(api.favorite(resource, id))
-    dispatch({ type: 'FAVORITE' })
+    dispatch({
+      type: resource === 'bundle' ? 'FAVORITE_BUNDLE' : 'FAVORITE_COLLECION',
+      id
+    })
   } 
 }
 
 export function unfavorite (resource, id) {
   return async (dispatch) => {
     let response = await request.delete(api.favorite(resource, id))
-    dispatch({ type: 'UNFAVORITE' })
+    dispatch({
+      type: resource === 'bundle' ? 'UNFAVORITE_BUNDLE' : 'UNFAVORITE_COLLECION',
+      id
+    })
   }
 }
--- src/reducers/Bundle.js
+++ src/reducers/Bundle.js
@@ -4,6 +4,21 @@ export default function (state = { list: [] }, action) {
       return { ...state, list: action.list }
     case 'RECEIVE_BUNDLE':
       return { ...state, current: action.bundle }
+    case 'FAVORITE_BUNDLE':
+      let newState = { ...state }
+      newState.list = newState.list.map(bundle => {
+        console.log(bundle.id, action.id)
+        if (bundle.id === action.id) bundle.favorited = true
+        return bundle
+      })
+      return newState
+    case 'UNFAVORITE_BUNDLE':
+      let _newState = { ...state }
+      _newState.list = _newState.list.map(bundle => {
+        if (bundle.id == action.id) bundle.favorited = false
+        return bundle
+      })
+      return _newState
     default:
       return state
   }
