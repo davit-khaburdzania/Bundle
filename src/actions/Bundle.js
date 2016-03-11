fdd54d7c44b73307182ee691d81db2bf17181c86
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-03-11 09:09:53 +0400

    add bundle remove action

--- src/actions/Bundle.js
+++ src/actions/Bundle.js
@@ -3,24 +3,21 @@ import api from './../api'
 
 export function getBundles () {
   return async (dispatch) => {
-    try {
-      let response = await request.get(api.bundles())
-      dispatch({ type: 'RECEIVE_BUNDLES', list: response.data })
-    } catch (error) {
-      // write error handler outside
-      console.log(error)
-    }
+    let response = await request.get(api.bundles())
+    dispatch({ type: 'RECEIVE_BUNDLES', list: response.data })
   }
 }
 
 export function getBundle (id) {
   return async (dispatch) => {
-    try {
-      let response = await request.get(api.bundles(id))
-      dispatch({ type: 'RECEIVE_BUNDLE', bundle: response.data })
-    } catch (error) {
-      // write error handler outside
-      console.log(error)
-    }
+    let response = await request.get(api.bundles(id))
+    dispatch({ type: 'RECEIVE_BUNDLE', bundle: response.data })
+  }
+}
+
+export function removeBundle (id) {
+  return async (dispatch) => {
+    let response = await request.delete(api.bundles(id))
+    dispatch({ type: 'DELETE_BUNDLE', id })
   }
 }
