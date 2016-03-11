4cb3c7f9dc4f3cde5181cdf7886d87a96cdfb77f
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-03-11 12:10:33 +0400

    action cleanups

--- src/actions/Collection.js
+++ src/actions/Collection.js
@@ -3,24 +3,14 @@ import api from './../api'
 
 export function getCollection (id) {
   return async (dispatch) => {
-    try {
-      let response = await request.get(api.collections(id))
-      dispatch({ type: 'RECEIVE_COLLECTION', collection: response.data })
-    } catch (error) {
-      // write error handler outside
-      console.log(error)
-    }
+    let response = await request.get(api.collections(id))
+    dispatch({ type: 'RECEIVE_COLLECTION', collection: response.data })
   }
 }
 
 export function getCollections () {
   return async (dispatch) => {
-    try {
-      let response = await request.get(api.collections())
-      dispatch({ type: 'RECEIVE_COLLECTIONS', list: response.data })
-    } catch (error) {
-      // write error handler outside
-      console.log(error)
-    }
+    let response = await request.get(api.collections())
+    dispatch({ type: 'RECEIVE_COLLECTIONS', list: response.data })
   }
 }
