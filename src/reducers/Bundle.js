cf77a7a7b7cef3d47283fbef72930af3598316a4
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-03-14 10:47:19 +0400

    changed switch case indentation

--- src/reducers/Bundle.js
+++ src/reducers/Bundle.js
@@ -1,52 +1,52 @@
 export default function (state = { list: [] }, action) {
   switch (action.type) {
-    case 'RECEIVE_BUNDLES':
-      return { ...state, list: action.list }
+  case 'RECEIVE_BUNDLES':
+    return { ...state, list: action.list }
 
-    case 'RECEIVE_BUNDLE':
-      return { ...state, current: action.bundle }
+  case 'RECEIVE_BUNDLE':
+    return { ...state, current: action.bundle }
 
-    case 'FAVORITE_BUNDLE':
-      var newState = { ...state }
-      newState.list = newState.list.map(bundle => {
-        if (bundle.id === action.id) bundle.favorited = true
-        return bundle
-      })
-      return newState
+  case 'FAVORITE_BUNDLE':
+    var newState = { ...state }
+    newState.list = newState.list.map(bundle => {
+      if (bundle.id === action.id) bundle.favorited = true
+      return bundle
+    })
+    return newState
 
-    case 'UNFAVORITE_BUNDLE':
-      var newState = { ...state }
-      newState.list = newState.list.map(bundle => {
-        if (bundle.id == action.id) bundle.favorited = false
-        return bundle
-      })
-      return newState
+  case 'UNFAVORITE_BUNDLE':
+    var newState = { ...state }
+    newState.list = newState.list.map(bundle => {
+      if (bundle.id == action.id) bundle.favorited = false
+      return bundle
+    })
+    return newState
 
-    case 'REMOVE_BUNDLE':
-      var newState = { ...state }
-      newState.list = newState.list.filter(bundle => bundle.id !== action.id)
-      return newState
+  case 'REMOVE_BUNDLE':
+    var newState = { ...state }
+    newState.list = newState.list.filter(bundle => bundle.id !== action.id)
+    return newState
 
-    case 'EDIT_MODE_BUNDLE':
-      var newState = { ...state }
-      newState.list = newState.list.map(bundle => {
-        if (bundle.id == action.id) bundle.editMode = action.editMode
-        return bundle
-      })
-      return newState
+  case 'EDIT_MODE_BUNDLE':
+    var newState = { ...state }
+    newState.list = newState.list.map(bundle => {
+      if (bundle.id == action.id) bundle.editMode = action.editMode
+      return bundle
+    })
+    return newState
 
-    case 'RENAME_BUNDLE':
-      var newState = { ...state }
-      newState.list = newState.list.map(bundle => {
-        if (bundle.id == action.id) {
-          bundle.name = action.name
-        }
+  case 'RENAME_BUNDLE':
+    var newState = { ...state }
+    newState.list = newState.list.map(bundle => {
+      if (bundle.id == action.id) {
+        bundle.name = action.name
+      }
 
-        return bundle
-      })
-      return newState
+      return bundle
+    })
+    return newState
 
-    default:
-      return state
+  default:
+    return state
   }
 }
--- src/reducers/Search.js
+++ src/reducers/Search.js
@@ -1,9 +1,9 @@
 export default function (state = {}, action) {
   switch (action.type) {
-    case 'FETCH_SEARCH_RESULTS':
-      let result = action.result
-      return { ...state, result }
-    default:
-      return state
+  case 'FETCH_SEARCH_RESULTS':
+    let result = action.result
+    return { ...state, result }
+  default:
+    return state
   }
 }
