ede57226ed9b99547c661d09d0bda1027cf8835c
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-03-15 17:35:44 +0400

    changed bundle actions

--- src/actions/Bundle.js
+++ src/actions/Bundle.js
@@ -1,17 +1,17 @@
 import request from 'axios'
 import api from './../api'
 
-export function getBundles () {
-  return async (dispatch) => {
-    let response = await request.get(api.bundles())
-    dispatch({ type: 'RECEIVE_BUNDLES', list: response.data })
-  }
-}
-
 export function getBundle (id) {
   return async (dispatch) => {
     let response = await request.get(api.bundles(id))
     dispatch({ type: 'RECEIVE_BUNDLE', bundle: response.data })
+  }
+}
+
+export function getBundles () {
+  return async (dispatch) => {
+    let response = await request.get(api.bundles())
+    dispatch({ type: 'RECEIVE_BUNDLES', list: response.data })
   }
 }
 
@@ -22,71 +22,47 @@ export function removeBundle (id) {
   }
 }
 
-export function editModeBundle (id, editMode) {
-  return { type: 'EDIT_MODE_BUNDLE', id, editMode }
+export function updateBundle (id, data) {
+  const payload = { bundle: data }
+
+  return async (dispatch) => {
+    let response = await request.put(api.bundles(id), payload)
+    dispatch({ type: 'UPDATE_BUNDLE', bundle: response.data })
+  }
 }
 
 export function renameBundle (id, name) {
   return async (dispatch) => {
     let response = await request.put(api.bundles(id), { name })
+
     dispatch({ type: 'RENAME_BUNDLE', id, name })
     dispatch(editModeBundle(id, false))
   }
 }
 
-export function updateBundle (bundleId, data) {
-  const payload = {
-    bundle: data
-  }
-
+export function fetchLink (url) {
   return async (dispatch) => {
-    try {
-      let response = await request.put(api.bundles(bundleId), payload)
-      dispatch({ type: 'UPDATE_BUNDLE', bundle: response.data })
-    } catch (error) {
-      // write error handler outside
-      console.log(error)
+    const response = await request.get(api.fetchLink(url))
+    const link = {
+      url: response.data.url,
+      title: response.data.title,
+      description: response.data.description,
+      image: response.data.image
     }
+
+    dispatch({ type: 'FETCH_LINK', link })
   }
 }
 
 export function updateBundleLink (id, field, value) {
   return (dispatch) => {
-    dispatch({
-      type: 'UPDATE_BUNDLE_LINK',
-      id,
-      field,
-      value
-    })
+    dispatch({ type: 'UPDATE_BUNDLE_LINK', id, field, value })
   }
 }
 
 export function updateBundleState(field, value) {
   return (dispatch) => {
-    dispatch({
-      type: 'UPDATE_BUNDLE_INFO',
-      field,
-      value
-    })
-  }
-}
-
-export function fetchLink (url) {
-  return async (dispatch) => {
-    try {
-      const response = await request.get(api.fetchLink(url))
-      const link = {
-        url: response.data.url,
-        title: response.data.title,
-        description: response.data.description,
-        image: response.data.image
-      }
-
-      dispatch({ type: 'FETCH_LINK', link })
-    } catch (error) {
-      // write error handler outside
-      console.log(error)
-    }
+    dispatch({ type: 'UPDATE_BUNDLE_INFO', field, value })
   }
 }
 
@@ -94,4 +70,8 @@ export function toggleEditMode () {
   return (dispatch) => {
     dispatch({ type: 'TOGGLE_EDIT_MODE' })
   }
+}
+
+export function editModeBundle (id, editMode) {
+  return { type: 'EDIT_MODE_BUNDLE', id, editMode }
 }
