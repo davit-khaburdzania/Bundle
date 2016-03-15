951c9a8d195cdb2be4b36c5ec82a3e24143f3884
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-03-15 17:46:06 +0400

    universal error handler

---
+++ src/store/middlewares/errorHandler.js
@@ -0,0 +1,24 @@
+import { applyMiddleware } from 'redux'
+import { addAlert } from './../../actions/Alert'
+
+function errorHandler () {
+  return next => action => {
+    if (typeof action === 'function') {
+      return next(async (dispatch, getState) => {
+        try {
+          return await action(dispatch, getState)
+        } catch (response) {
+          if (response instanceof Error) {
+            return dispatch(addAlert('error', response.message))
+          } else {
+            return dispatch(addAlert('error', response.data.errors || response.statusText))
+          }
+        }
+      })
+    } else {
+      return next(action)
+    }
+  }
+}
+
+export default () => applyMiddleware(errorHandler)
