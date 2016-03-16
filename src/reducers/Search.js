f637027bdb2f3d37e7d0d8dcba325fdec3194749
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-03-16 21:50:43 +0400

    fixed minor bug

--- src/reducers/Search.js
+++ src/reducers/Search.js
@@ -1,6 +1,6 @@
 import Immutable from 'immutable'
 
-export default function (state = Immutable.Map({}), action) {
+export default function (state = Immutable.Map(), action) {
   switch (action.type) {
   case 'FETCH_SEARCH_RESULTS':
     return state.set('result', Immutable.fromJS(action.result))
