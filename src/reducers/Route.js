6dfca92ca7f87dc0aa204cb63c1ede3f44ca2f32
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-04-10 13:47:44 +0800

    route actions and reducers

---
+++ src/reducers/Route.js
@@ -0,0 +1,19 @@
+import { fromJS } from 'immutable'
+
+let defaultState = fromJS({
+  bundle: { id: null },
+  navigation: { view: 'bundles', collectionId: null}
+})
+
+export default function (state = defaultState, action) {
+  switch (action.type) {
+    case 'ROUTE_CHANGE_BUNDLE_ID':
+      return state.setIn(['bundle', 'id'], action.bundleID)
+    case 'ROUTE_CHANGE_NAVIGATION_VIEW':
+      return state.setIn(['navigation', 'view'], action.view)
+    case 'ROUTE_CHANGE_NAVIGATION_COLLECTION_ID':
+      return state.setIn(['navigation', 'collectionId'], action.collectionId)
+    default:
+      return state
+  }
+}
