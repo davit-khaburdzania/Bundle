dd4367be8fb482d61d86cf96ee886955cca6a0c2
gulisashvili <levan.gulisashvili@gmail.com>
2016-04-20 20:01:14 +0400

    removed unnessesory func call in favorite reducer

--- src/reducers/Favorite.js
+++ src/reducers/Favorite.js
@@ -1,9 +1,9 @@
-import { Map, fromJS } from 'immutable'
+import { Map } from 'immutable'
 
 export default function (state = Map(), action) {
   switch (action.type) {
     case 'RECEIVE_FAVORITES_LIST':
-      return state.set('list', fromJS(action.list))
+      return state.set('list', action.list)
 
     default:
       return state
