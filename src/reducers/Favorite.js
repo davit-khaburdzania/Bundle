fc40bc8fd6a0370095c5eb226b0dd4e117054787
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-04-21 11:05:12 +0800

    fixed possible favorite dublication issue

--- src/reducers/Favorite.js
+++ src/reducers/Favorite.js
@@ -7,10 +7,12 @@ export default function (state = defaultState, action) {
   switch (action.type) {
     case 'RECEIVE_FAVORITES':
       action.list.forEach(favorite => {
-        state = state.setIn(['byId', favorite.get('id')], favorite)
+        let id = favorite.get('id') + '-' + favorite.get('type')
+        state = state.setIn(['byId', id], favorite)
       })
 
       return state
+
     default:
       return state
   }
