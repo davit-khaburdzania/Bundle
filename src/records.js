ebb4f2774202f60ecfb76b1e49fdffe3826f2c3e
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-04-26 16:23:55 +0800

    changed undefineds to null

--- src/records.js
+++ src/records.js
@@ -1,48 +1,48 @@
 import { Record, Map } from 'immutable'
 
 export const User = Record({
-  id: undefined,
-  name: undefined,
-  email: undefined,
-  image: undefined,
-  auth_token: undefined
+  id: null,
+  name: null,
+  email: null,
+  image: null,
+  auth_token: null
 })
 
 export const Bundle = Record({
-  id: undefined,
-  name: undefined,
-  description: undefined,
-  creator: undefined,
-  links: undefined,
-  links_count: undefined,
-  favorited: undefined,
-  favorites_count: undefined,
+  id: null,
+  name: null,
+  description: null,
+  creator: null,
+  links: null,
+  links_count: null,
+  favorited: null,
+  favorites_count: null,
   full_response: false,
-  isNewBundle: undefined,
+  isNewBundle: null,
   editMode: false,
-  created_at: undefined
+  created_at: null
 })
 
 export const Collection = Record({
-  id: undefined,
-  name: undefined,
-  bundles_count: undefined,
-  favorites_count: undefined,
-  shares_count: undefined,
-  favorited: undefined,
-  bundles: undefined,
+  id: null,
+  name: null,
+  bundles_count: null,
+  favorites_count: null,
+  shares_count: null,
+  favorited: null,
+  bundles: null,
   full_response: false,
-  created_at: undefined
+  created_at: null
 })
 
 export const Link = Record({
-  id: undefined,
-  title: undefined,
-  description: undefined,
-  url: undefined,
-  image: undefined,
-  creator: undefined,
-  created_at: undefined
+  id: null,
+  title: null,
+  description: null,
+  url: null,
+  image: null,
+  creator: null,
+  created_at: null
 })
 
 
