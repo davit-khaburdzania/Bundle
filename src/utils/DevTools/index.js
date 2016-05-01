8c33304368fb6721f81d274f9018a11b6e15634a
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-05-01 18:39:28 +0800

    removed unuseful devtools extensions, updated inspector for immutable support

--- src/utils/DevTools/index.js
+++ src/utils/DevTools/index.js
@@ -2,26 +2,14 @@ import React from 'react'
 import { createDevTools } from 'redux-devtools'
 
 import DockMonitor from 'redux-devtools-dock-monitor'
-import MultipleMonitors from 'redux-devtools-multiple-monitors'
 import Inspector from 'redux-devtools-inspector'
-import LogMonitor from 'redux-devtools-log-monitor'
-import Dispatcher from 'redux-devtools-dispatch'
-import SliderMonitor from 'redux-slider-monitor'
-
-import { allActions } from '../../actions'
 
 export default createDevTools(
   <DockMonitor
     toggleVisibilityKey='ctrl-h'
     changePositionKey='ctrl-q'
-    changeMonitorKey='ctrl-m'
-    defaultIsVisible={false}
+    defaultPosition='bottom'
   >
-    <Inspector />
-    <MultipleMonitors>
-      <LogMonitor />
-      <Dispatcher actionCreators={allActions} />
-    </MultipleMonitors>
-    <SliderMonitor keyboardEnabled />
+    <Inspector supportImmutable={true}/>
   </DockMonitor>
 )
