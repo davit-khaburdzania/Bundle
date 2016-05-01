7a1b2dac040366a2058f00ac3082cfff7d85887a
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-05-01 13:00:12 +0800

    confirm deletetion for bundles

--- src/components/ListToolbar/ToolbarDeleteItem/index.js
+++ src/components/ListToolbar/ToolbarDeleteItem/index.js
@@ -1,10 +1,17 @@
-export default function ToolbarDeleteItem ({
-  id,
-  remove
-}) {
-  return (
-    <div className='icon icon-toolbar-delete'
-      onClick={remove.bind(this, id)}
-    />
-  )
+export default class ToolbarDeleteItem extends React.Component {
+  delete (id) {
+    if (confirm('are you sure?')) {
+      this.props.remove(id)
+    }
+  }
+
+  render () {
+    let { id } = this.props
+
+    return (
+      <div className='icon icon-toolbar-delete'
+        onClick={this.delete.bind(this, id)}
+      />
+    )
+  }
 }
